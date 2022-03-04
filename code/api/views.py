4959a8587fdf881fca6f90b69e8f3b3911f9b135
klawik-j <klawik.j@gmail.com>
2022-03-04 22:00:31 +0100

    [Issue #9]

--- code/api/views.py
+++ code/api/views.py
@@ -1,12 +1,7 @@
-from django.http import JsonResponse, HttpResponse
-
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
-from .utils.utils import solve_puzzle, EMPTY_PUZZLE
-from .utils.board import Board
-from .utils.exceptions import BaseOCRException
+from sudoku_ocr import Board
 
 # Create your views here.
 
@@ -16,34 +11,34 @@ class SolveView(APIView):
 
     def post(self, request, format=None):
         puzzle = request.data["puzzle"]
-        solved_puzzle = solve_puzzle(puzzle)
-        if not 1 in solved_puzzle[0]:
-            return Response({
-                "solved_puzzle": EMPTY_PUZZLE,
-                "error": "sudoku not valid",
-                })
-        else:
-            return Response({
-                "solved_puzzle": solved_puzzle,
-                "error": ""})
-
-class OCRView(APIView):
-
-    # permission_classes = [IsAuthenticated] TBD v1.0.0
-
-    def post(self, request, format=None):
-        if not 'image' in request.data.keys():
-            return Response({"error": "no image included"})
         board = Board()
-        board.prepare_img_from_data(request.data['image'].read())
-        board.load_SNN_model("cnn")
-        try:
-            board.ocr_sudoku()
-        except BaseOCRException as err:
-            return Response({
-                "puzzle": EMPTY_PUZZLE,
-                "error": f"{err}"})
-            
+        board.board_value = puzzle
+        board.solve()
+        error_message = ""
+        if not 1 in board.solved_board[0]:
+            error_message = "sudoku not valid"
         return Response({
-            "puzzle": board.value,
-            "error": ""})
+                "solved_puzzle": board.solved_board,
+                "error": error_message,
+                })
+
+# class OCRView(APIView):
+
+#     # permission_classes = [IsAuthenticated] TBD v1.0.0
+
+#     def post(self, request, format=None):
+#         if not 'image' in request.data.keys():
+#             return Response({"error": "no image included"})
+#         board = Board()
+#         board.prepare_img_from_data(request.data['image'].read())
+#         board.load_SNN_model("cnn")
+#         try:
+#             board.ocr_sudoku()
+#         except BaseOCRException as err:
+#             return Response({
+#                 "puzzle": EMPTY_PUZZLE,
+#                 "error": f"{err}"})
+            
+#         return Response({
+#             "puzzle": board.value,
+#             "error": ""})
